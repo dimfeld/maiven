97d06b743bd2efbe661eaf897e8ecbbaaf298962
Daniel Imfeld <daniel@imfeld.dev>
2023-05-22 08:17:37 +0000

    add superforms

--- web/src/routes/+page.server.ts
+++ web/src/routes/+page.server.ts
@@ -1,22 +1,28 @@
 import { env } from '$env/dynamic/private';
 import { fail } from '@sveltejs/kit';
 import ky from 'ky-universal';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
 
 const OPENAI_URL = env.OPENAI_URL || 'https://api.openai.com/v1/chat/completions';
 
+const chatSchema = z.object({
+  input: z.string().nonempty(),
+});
+
 export async function load() {
   return {
-    messages: [] as string[],
+    form: await superValidate(chatSchema),
+    messages: [] as { role: 'user' | 'bot'; message: string }[],
   };
 }
 
 export const actions = {
   default: async ({ fetch, request }) => {
-    const data = await request.formData();
-    const input = data.get('input')?.toString().trim();
+    const form = await superValidate(request, chatSchema);
 
-    if (!input) {
-      return fail(400, { error: 'Chat is empty' });
+    if (!form.valid) {
+      return fail(400, { form });
     }
 
     const response = await ky(OPENAI_URL, {
@@ -25,18 +31,20 @@ export const actions = {
       headers: {
         Authorization: `Bearer ${env.OPENAI_TOKEN}`,
       },
+      timeout: 30000,
       json: {
         model: 'gpt-3.5-turbo',
         messages: [
           {
             role: 'user',
-            content: input,
+            content: form.data.input,
           },
         ],
       },
     }).json();
 
     return {
+      form,
       response: response.choices[0].message.content as string,
     };
   },
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,57 +1,85 @@
 <script lang="ts">
-  import { enhance, applyAction } from '$app/forms';
-  import type { SubmitFunction } from '$app/forms';
+  import { superForm } from 'sveltekit-superforms/client';
+  import { fly } from 'svelte/transition';
 
   export let data;
-  export let form;
 
-  const handleChatSubmit: SubmitFunction = ({ cancel, formElement, formData }) => {
-    let input = formData.get('input');
-    if (!input) {
-      cancel();
-      return;
-    }
+  const { form, constraints, enhance, errors, delayed } = superForm(data.form, {
+    invalidateAll: false,
+    resetForm: false,
+    taintedMessage: false,
+    timeoutMs: 30000,
+  });
 
-    data.messages = [...data.messages, input];
-
-    return async ({ result }) => {
-      applyAction(result);
-      if (result.type == 'success') {
-        formElement.reset();
-        chatInput.focus();
-
-        let response = result.data?.response;
-        if (response) {
-          data.messages = [...data.messages, response];
-        }
-      }
-    };
-  };
+  function addMessage(role: string, message: string) {
+    data.messages = [...data.messages, { role, message }];
+  }
 
   let chatInput: HTMLTextAreaElement;
+  let chatSubmit: HTMLButtonElement;
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-center items-center px-4">
   <div class="space-y-5">
-    {#if form?.error}
-      <div class="alert variant-filled-error">
-        {form.error}
+    {#each data.messages as message}
+      <div
+        class="card p-2"
+        in:fly|local={{
+          x: message.role === 'bot' ? 100 : -100,
+        }}
+      >
+        {message.role}: {message.message}
       </div>
+    {/each}
+    {#if $delayed}
+      <div class="p-2">Loading...</div>
     {/if}
-    <form method="POST" use:enhance={handleChatSubmit}>
+    <form
+      method="POST"
+      use:enhance={{
+        onSubmit({ formData, cancel }) {
+          const input = formData.get('input');
+          if (typeof input !== 'string' || !input.trim().length) {
+            cancel();
+            chatInput.focus();
+            return;
+          }
+          addMessage('user', input);
+        },
+        onResult({ result }) {
+          if (result.type == 'success') {
+            addMessage('bot', result.data?.response);
+          }
+        },
+        onUpdate({ form }) {
+          if (form.valid) {
+            form.data.input = '';
+          }
+        },
+        onUpdated() {
+          setTimeout(() => {
+            console.log(document.activeElement);
+            if (document.activeElement === chatSubmit || document.activeElement === document.body) {
+              chatInput.focus();
+            }
+          });
+        },
+      }}
+    >
       <textarea
         bind:this={chatInput}
         name="input"
         class="textarea"
-        required
-        value={form?.input ?? ''}
+        data-invalid={$errors.input}
+        bind:value={$form.input}
+        on:keydown={(e) => {
+          if (e.key === 'Enter' && e.metaKey) {
+            e.preventDefault();
+            e.currentTarget.form?.requestSubmit();
+          }
+        }}
       />
-      <button type="submit" class="btn variant-filled-primary">Send</button>
+      <button bind:this={chatSubmit} type="submit" class="btn variant-filled-primary">Send</button>
     </form>
-    {#each data.messages as message}
-      <div class="card">
-        {message}
-      </div>
-    {/each}
   </div>
 </div>
