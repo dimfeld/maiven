7d91581d38261a729a930a427ba1458fced80e01
Daniel Imfeld <daniel@imfeld.dev>
2023-06-11 15:09:37 -0700

    Allow SvelteKit to read .env from repository root

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,10 +11,11 @@ const config = {
     inspector: true,
   },
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    env: {
+      // Get the .env file from the repo root
+      dir: '..',
+    },
   },
 };
 export default config;
